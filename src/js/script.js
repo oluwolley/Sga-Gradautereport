64224463f398d4a429a537b223cf1f7b0cc9bd8c
hammed01 <shotolahammed01@gmail.com>
2018-10-13 11:21:23 +0100

    Adjusted the mobile view vh

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,13 @@
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
+
+
 $(function(){
     
    $('.tab-list').each(function(){                   // Find lists of tabs
