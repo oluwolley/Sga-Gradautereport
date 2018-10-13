7faf7415c7724c46ffd96bad517e26b8cf12386b
hammed01 <shotolahammed01@gmail.com>
2018-10-13 10:06:31 +0100

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,59 @@
+$(function(){
+    
+   $('.tab-list').each(function(){                   // Find lists of tabs
+  var $this = $(this);                            // Store this list
+  var $tab = $this.find('li.active');             // Get the active list item
+  var $link = $tab.find('a');                     // Get link from active tab
+  var $panel = $($link.attr('href'));             // Get active panel
+
+  $this.on('click', '.tab-control', function(e) { // When click on a tab
+    e.preventDefault();                           // Prevent link behavior
+    var $link = $(this),                          // Store the current link
+        id = this.hash;                           // Get href of clicked tab 
+
+    if (id && !$link.is('.active')) {             // If not currently active
+      $panel.removeClass('active');               // Make panel inactive
+      $tab.removeClass('active');                 // Make tab inactive
+
+      $panel = $(id).addClass('active');          // Make new panel active
+      $tab = $link.parent().addClass('active');   // Make new tab active 
+    }
+  })
+   })
+    
+     $(document).ready(function(){
+    $('.your-class').slick({
+      arrows:false,
+      dots:false,
+      autoplay:true,
+      adaptiveHeight:true,
+      infinite: true,
+      centerMode:true,
+      slidesToShow: 3,
+     slidesToScroll: 3
+    });
+  });
+
+    
+        
+ $(".main").onepage_scroll({
+    sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+    easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                     // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+    animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+    pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+    updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+    beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+    afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+    loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+    keyboard: true,                  // You can activate the keyboard controls
+    responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+                                     // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                     // the browser's width is less than 600, the fallback will kick in.
+    direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+ });
+    
+   
+});
+
+
